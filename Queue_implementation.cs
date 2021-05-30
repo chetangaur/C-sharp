10ce03c43726a7d183ab3a2a944bf7d582188fe2
Chetan <43693378+chetangaur@users.noreply.github.com>
2021-05-30 14:43:40 +0530

    Implementation of queue using array

---
+++ Queue_implementation.cs
@@ -0,0 +1,29 @@
+
+Result Size: 1516 x 297
+            {
+                for(int i=front;i<=rear;i++)
+                {
+                    Console.WriteLine("{0}",array_1[i]);
+                    //return;
+                }
+            }
+        }
+    class driving_prog
+        {
+            static void Main()
+            {
+                queue Q = new queue(5);
+                Q.enqueue(10);
+                Q.enqueue(20);
+                Q.enqueue(30);
+                Q.enqueue(40);
+                Q.enqueue(50);
+                //Q.print_queue();
+                Q.deque();
+                Q.print_queue();
+                
+            }
+        }
+    }
+ }
+   
